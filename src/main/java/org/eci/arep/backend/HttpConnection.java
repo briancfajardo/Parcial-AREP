286b2ae795641c859d8dd1ce7cc9b5cdea66618a
unknown <brian.fajardo@labinfo.is.escuelaing.edu.co>
2024-02-22 15:41:46 -0500

    Los dos servicios están conectados, la fachada, con el api

--- src/main/java/org/eci/arep/backend/HttpConnection.java
+++ src/main/java/org/eci/arep/backend/HttpConnection.java
@@ -3,16 +3,93 @@ package org.eci.arep.backend;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.PrintWriter;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class HttpConnection {
 
     private static final String USER_AGENT = "Mozilla/5.0";
-    private static final String GET_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=fb&apikey=Q1QZFVJQ21K7C6XM";
+    private static final String GET_URL = "http://localhost:36000/method=";
 
     public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(35000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+        while (true){
+            Socket clientSocket = null;
 
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Recibí: " + inputLine);
+                if(inputLine.contains("GET")){
+                    if(inputLine.contains("compreflex")){
+                        outputLine = response();
+                    }else{
+                        outputLine = error();
+                    }
+                out.println(outputLine);
+                }
+
+                if (!in.ready()) {break; }
+            }
+            out.close();
+            in.close();
+            clientSocket.close();
+
+        }
+
+
+    }
+    public static String response(){
+        return "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n"
+                + "<title>All good</title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<h1>All good</h1>\n"
+                + "</body>\n"
+                + "</html>\n";
+    }
+    public static String error(){
+        return "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n"
+                + "<title>Error, intente de nuevo</title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<h1>Error, intente de nuevo</h1>\n"
+                + "</body>\n"
+                + "</html>\n";
+    }
+
+    public void getEx() throws IOException {
         URL obj = new URL(GET_URL);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
@@ -23,8 +100,7 @@ public class HttpConnection {
         System.out.println("GET Response Code :: " + responseCode);
 
         if (responseCode == HttpURLConnection.HTTP_OK) { // success
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    con.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
             StringBuffer response = new StringBuffer();
 
--- src/main/java/org/eci/arep/facade/HttpServer.java
+++ src/main/java/org/eci/arep/facade/HttpServer.java
@@ -2,35 +2,101 @@ package org.eci.arep.facade;
 
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class HttpServer {
+
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_URL = "http://localhost:35000";
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         try {
             serverSocket = new ServerSocket(36000);
         } catch (IOException e) {
-            System.err.println("Could not listen on port: 36000.");
+            System.err.println("Could not listen on port: 35000.");
             System.exit(1);
         }
 
-        Socket clientSocket = null;
-        try {
-            System.out.println("Listo para recibir ...");
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
+        while (true){
+            Socket clientSocket = null;
+
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Recibí: " + inputLine);
+                ArrayList<String> listInLine = new ArrayList<>(List.of(inputLine.split(" ")));
+                if(listInLine.size() > 1 && Objects.equals(listInLine.get(0), "GET")){
+
+                    if(Objects.equals(listInLine.get(1), "/cliente")){
+                        outputLine = HttpIndex();
+                    }else{
+                        outputLine = sendMessage(listInLine.get(1));
+                    }
+                    out.println(outputLine);
+                }
+                if (!in.ready()) {break; }
+            }
+            out.close();
+            in.close();
+            clientSocket.close();
         }
-        PrintWriter out = new PrintWriter(
-                clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(clientSocket.getInputStream()));
-        String inputLine, outputLine;
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println("Recibí: " + inputLine);
-            if (!in.ready()) {break; }
+    }
+
+    public static String sendMessage(String method) throws IOException {
+        String header;
+        String body = "";
+        String output;
+        header = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/json\r\n"
+                + "\r\n";
+
+        if(method.contains("compreflex")){
+            URL obj = new URL(GET_URL+ method);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            //The following invocation perform the connection implicitly before getting the code
+            int responseCode = con.getResponseCode();
+            System.out.println("GET Response Code :: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                body += response;
+                // print result
+                System.out.println(response.toString());
+            } else {
+                System.out.println("GET request not worked");
+            }
+            System.out.println("GET DONE");
         }
-        outputLine = "HTTP/1.1 200 OK\r\n"
+
+        output = header + body;
+        return output;
+
+    }
+    public static String HttpIndex(){
+        String outputLine;
+        return outputLine = "HTTP/1.1 200 OK\r\n"
                 + "Content-Type: text/html\r\n"
                 + "\r\n"
                 + "<!DOCTYPE html>\n" +
@@ -41,10 +107,10 @@ public class HttpServer {
                 "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
                 "    </head>\n" +
                 "    <body>\n" +
-                "        <h1>Form with GET</h1>\n" +
-                "        <form action=\"/hello\">\n" +
-                "            <label for=\"name\">Name:</label><br>\n" +
-                "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\n" +
+                "        <h1>Reflective ChatGPT</h1>\n" +
+                "        <form action=\"/func\">\n" +
+                "            <label for=\"name\">Método que desea usar:</label><br>\n" +
+                "            <input type=\"text\" id=\"name\" name=\"name\" value=\"java.lang.System, getenv\"><br><br>\n" +
                 "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n" +
                 "        </form> \n" +
                 "        <div id=\"getrespmsg\"></div>\n" +
@@ -57,35 +123,12 @@ public class HttpServer {
                 "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
                 "                    this.responseText;\n" +
                 "                }\n" +
-                "                xhttp.open(\"GET\", \"/hello?name=\"+nameVar);\n" +
+                "                xhttp.open(\"GET\", \"/compreflex?comando=\"+nameVar);\n" +
                 "                xhttp.send();\n" +
                 "            }\n" +
                 "        </script>\n" +
                 "\n" +
-                "        <h1>Form with POST</h1>\n" +
-                "        <form action=\"/hellopost\">\n" +
-                "            <label for=\"postname\">Name:</label><br>\n" +
-                "            <input type=\"text\" id=\"postname\" name=\"name\" value=\"John\"><br><br>\n" +
-                "            <input type=\"button\" value=\"Submit\" onclick=\"loadPostMsg(postname)\">\n" +
-                "        </form>\n" +
-                "        \n" +
-                "        <div id=\"postrespmsg\"></div>\n" +
-                "        \n" +
-                "        <script>\n" +
-                "            function loadPostMsg(name){\n" +
-                "                let url = \"/hellopost?name=\" + name.value;\n" +
-                "\n" +
-                "                fetch (url, {method: 'POST'})\n" +
-                "                    .then(x => x.text())\n" +
-                "                    .then(y => document.getElementById(\"postrespmsg\").innerHTML = y);\n" +
-                "            }\n" +
-                "        </script>\n" +
                 "    </body>\n" +
                 "</html>";
-        out.println(outputLine);
-        out.close();
-        in.close();
-        clientSocket.close();
-        serverSocket.close();
     }
 }
